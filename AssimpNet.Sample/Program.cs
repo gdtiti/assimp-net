18068052470752bac7697476830a4bff1530dc27
Amer Koleci <amerkoleci@gmail.com>
2018-03-06 20:45:02 +0100

    More cleanup and convert sample into net standard 2.0 application.

--- AssimpNet.Sample/Program.cs
+++ AssimpNet.Sample/Program.cs
@@ -24,16 +24,13 @@ using System;
 
 namespace Assimp.Sample
 {
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            SimpleOpenGLSample sample = new SimpleOpenGLSample();
-            sample.Run(30.0, 0.0);
-        }
-    }
+	internal static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			SimpleOpenGLSample sample = new SimpleOpenGLSample();
+			sample.Run(30.0, 0.0);
+		}
+	}
 }
